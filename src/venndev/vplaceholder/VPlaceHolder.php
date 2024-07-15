9e4e463a66fd9d908f787675ed9c160c0d81c75f
VennV <111500380+VennDev@users.noreply.github.com>
2024-07-15 23:28:52 +0700

    Update ModuleManager for virion!
    Now you can do this: 
    You need create one file NameModule.php in folder generated by virions when one other plugin `init` virion this!
    ```php
    <?php
    
    declare(strict_types=1);
    
    use venndev\vplaceholder\VPlaceHolder;
    
    VPlaceHolder::registerPlaceHolder("{player_name}", function (string $player): string {
        return $player;
    });
    ```

--- src/venndev/vplaceholder/VPlaceHolder.php
+++ src/venndev/vplaceholder/VPlaceHolder.php
@@ -6,16 +6,20 @@ namespace venndev\vplaceholder;
 
 use pocketmine\plugin\PluginBase;
 use venndev\vplaceholder\handler\PlaceHolderHandler;
+use venndev\vplaceholder\manager\ModuleManager;
 
 final class VPlaceHolder
 {
     use PlaceHolderHandler;
+    use ModuleManager;
 
     private static bool $enabled = false;
 
     public static function init(PluginBase $plugin): void
     {
         if (self::$enabled) return;
+        self::initModules($plugin);
+        self::runModules();
         $plugin->getServer()->getPluginManager()->registerEvents(new listener\EventListener(), $plugin);
         self::$enabled = true;
     }
---
+++ src/venndev/vplaceholder/manager/ModuleManager.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace venndev\vplaceholder\manager;
+
+use pocketmine\plugin\PluginBase;
+
+trait ModuleManager
+{
+
+    private static string $modulePath = "modules_placeholder";
+
+    private static function initModules(PluginBase $plugin): void
+    {
+        self::$modulePath = $plugin->getServer()->getDataPath() . DIRECTORY_SEPARATOR . "modules_placeholder";
+        if (!is_dir(self::$modulePath)) @mkdir(self::$modulePath);
+    }
+
+    public static function runModule(string $module): void
+    {
+        if (!file_exists(self::$modulePath . DIRECTORY_SEPARATOR . $module)) return;
+        require_once self::$modulePath . DIRECTORY_SEPARATOR . $module;
+    }
+
+    public static function runModules(): void
+    {
+        foreach (scandir(self::$modulePath) as $module) {
+            if ($module === "." || $module === "..") continue;
+            self::runModule($module);
+        }
+    }
+
+}
